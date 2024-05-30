9289541230e12a3854405838233b90ae212a3b43
bradzara <bradzara93@gmail.com>
2024-05-30 09:57:02 -0700

    Add HTML content

--- src/ProductsIndex.jsx
+++ src/ProductsIndex.jsx
@@ -1,8 +1,8 @@
 export function ProductsIndex() {
   return (
     <div id="products-index">
-      <h2>Whatever</h2>
-      <p2>Whatever, Whatever, Whatever, Whatever, Whatever.</p2>
+      <h2>Whatever</h2><br />
+      <p2>Whatever, Whatever, Whatever, Whatever, Whatever.</p2><br />
       <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdZbXU5MsUI9n0gWVqHnihxS3ZVPBvF0SyqA&s" />
     </div>
   );
--- src/ProductsNew.jsx
+++ src/ProductsNew.jsx
@@ -1,8 +1,8 @@
 export function ProductsNew() {
   return (
     <div id="products-new">
-      <h1>Whatever</h1>
-      <p1>Whatever</p1>
+      <h1>Whatever</h1><br />
+      <p1>Whatever</p1><br />
       <img src="https://t3.ftcdn.net/jpg/03/12/78/12/360_F_312781243_PqzPEMlBTYtjew947z0xSHbuwlaUHbbW.jpg" />
     </div>
   );
